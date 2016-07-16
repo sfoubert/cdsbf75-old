08a61c4e4e1f7294bcb7c792c1e6a1e0e42fdffb
Sebastien Foubert <sebastien.foubert@gmail.com>
2016-07-16 22:10:08 +0200

    Add analytics

--- cdsbf75/config.js
+++ cdsbf75/config.js
@@ -45,7 +45,7 @@ var config = {
   image_url: '/images',
 
   // Add your analytics tracking code (including script tags)
-  analytics: '',
+  analytics: "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-2767287-18', 'auto');ga('send', 'pageview');</script>",
 
   // Set to true to enable the web editor
   allow_editing : true,
